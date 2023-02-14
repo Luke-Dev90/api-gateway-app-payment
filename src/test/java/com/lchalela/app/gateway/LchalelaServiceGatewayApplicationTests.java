85dae2de23cb79087f7afb6c6a2724409240c4c9
lucaschalela990 <lukee_sf@hotmail.com>
2023-02-14 09:48:38 -0300

    init eureka server

---
+++ src/test/java/com/lchalela/app/gateway/LchalelaServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lchalela.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LchalelaServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
